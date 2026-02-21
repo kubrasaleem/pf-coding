2368df1262905b050b3478926ef848c39572935b
Kubra Saleem <akhtarkhan71590@gmail.com>
2026-02-20 23:05:48 -0800

    Add files via upload

---
+++ gameplayer.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+ main (){
+cout<<"          ___________________                  "<<endl;
+cout<<"       //  ________________ \\                 "<<endl;
+cout<<"      // //                \\ \\               "<<endl;              
+cout<<" -----   ________________/)--\\ \\---------    "<<endl; 
+cout<<" |                                         \\  "<<endl;
+cout<<" |                                          \\ "<<endl;
+cout<<" _                                            |"<<endl;
+cout<<"| |                                           |"<<endl;
+cout<<"|_|                                           |"<<endl;                                             
+cout<<" |  .'---'.  _______________________.'--.'____|"<<endl;
+cout<<"    (     )                         (    )     "<<endl;
+cout<<"    '.---.'                         '.--.'     ";
+
+     
+
+
+}
