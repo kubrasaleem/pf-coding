1e1c6f053b0d4078a01e7e43a754d810f535e85e
Kubra Saleem <kubrasaleem26.2026@gmail.com>
2026-03-01 03:34:44 -0800

    Correct summation of five integers in hometask13.cpp
    Fix input and summation logic for five integers.

---
+++ hometask13.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+main(){
+int num;
+int sum = 0;
+cout<<"Enter first integer";
+cin>>num;
+sum = sum+num;
+cout<<"Enter second integer";
+cin>>num;
+sum=sum+num;
+cout<<"Enter third integer";
+cin>>num;
+sum=sum+num;
+cout<<"Enter fourth integer";
+cin>>num;
+sum=sum+num;
+cout<<"Enter fifth integer";
+cin>>num;
+sum=sum+num;
+cout<<sum<<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+}
